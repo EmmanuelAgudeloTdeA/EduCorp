cb2a55cabba2a2d8932678469054357bf9549ce5
Emmanuel Agudelo Gaviria <emmanuel.agudelo@sensoria.city>
2025-10-20 17:44:21 -0500

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArj-AUlB8YDDnVXcSgdffDCqn0NKuVBGA",
+  authDomain: "educorp-67222.firebaseapp.com",
+  projectId: "educorp-67222",
+  storageBucket: "educorp-67222.firebasestorage.app",
+  messagingSenderId: "957217691651",
+  appId: "1:957217691651:web:e8ca9a5d70655477e21636"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
